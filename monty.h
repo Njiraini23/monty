a8e84ac2cb804e8fd97909cfb7ede96abfd49686
Njiraini23 <njiraini88@gmail.com>
2023-01-19 13:46:22 +0000

    monty.h file fixes

--- monty.h
+++ monty.h
@@ -47,6 +47,7 @@ int empty_line(char *line, char *delims);
 
 int execute(char **token, stack_t **stack, unsigned int line_num);
 int monty_push(stack_t **stack, char **token, unsigned int line_num);
+monty_pushq(&stack, token, line_num);
 int monty_pushq(stack_t **stack, char **token, unsigned int line_num);
 void monty_pall(stack_t **stack, unsigned int line_num);
 
