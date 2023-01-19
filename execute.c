32f19802971605d6184a0416e6b114403f752639
vpnchengo <charo.alvin@gmail.com>
2023-01-19 09:33:55 +0000

    upadte1

---
+++ execute.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * execute - Function that checks for a command "opcode" and execute the
+ * function if there's a match.
+ * @token: Tokane containing the opcode to execute.
+ * @stack: Doubly linked list representation of a stack.
+ * @line_num: Line number where the opcode was found.
+ * Return: EXIT_SUCCESS in success, or EXIT_FAILURE on failure.
+ */
+
+int execute(char **token, stack_t **stack, unsigned int line_num)
+{
+	unsigned int i = 0;
+
+	instruction_t op[] = {
+		{"pall", monty_pall},
+		{"pint", monty_pint},
+		{"pop", monty_pop},
+		{"swap", monty_swap},
+		{"add", monty_add},
+		{"sub", monty_sub},
+		{"mul", monty_mul},
+		{"div", monty_div},
+		{"mod", monty_mod},
+		{"pchar", monty_pchar},
+		{"pstr", monty_pstr},
+		{"rotl", monty_rotl},
+		{"rotr", monty_rotr},
+		{"null", NULL}
+	};
+
+	for (i = 0; i < 14; i++)
+	{
+		if (strcmp(op[i].opcode, token[0]) == 0)
+		{
+			op[i].f(stack, line_num);
+			return (EXIT_SUCCESS);
+		}
+	}
+	free_stack(stack);
+	fprintf(stderr, "L%i: unknown instruction %s\n", line_num, token[0]);
+	return (EXIT_FAILURE);
+}
---
+++ free.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * free_stack - A function that frees the memory allocated for stack.
+ * @stack: Pointer to the doubly linked list representation of a stack.
+ * Return: Nothing.
+ */
+
+void free_stack(stack_t **stack)
+{
+	stack_t *tmp = *stack;
+
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+}
